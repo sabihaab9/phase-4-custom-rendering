535e14f05275928bf3ea9516f5db430c2857d8d9
Sabiha Bary <sabihaab9@gmail.com>
2022-07-31 13:17:14 -0400

    commit tree dadf1a0a8b41f748f7ad0a91988b7385dbc42dfa

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,15 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+     render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+    }
+
+    #render json: cheese, only: [:id, :name, :price, :is_best_seller]
   end
+  
 
 end
